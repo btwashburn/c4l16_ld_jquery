5490d8093cf676f66541e1630559ff9ec91c63d4
Bruce Washburn <btwashburn@gmail.com>
2016-02-23 09:32:32 -0800

    Add checks for non-integer OCLC number values, and integer values that do not return a valid graph

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@
       var kb;
       var uri;
       var request_url;
+      var properties = ["name","creator","about","description"];
       var labels = {};
       labels.name = "Title";
       labels.creator = "Author";
@@ -17,6 +18,11 @@
         // create an RDF graph
         kb = $rdf.graph();
         
+        // remove previously-set values in the UI
+        $.each(properties, function( pindex, prop ) {
+          $("#"+prop).html("");
+        });
+        
         // get RDF data
         fetchRdf();
         
@@ -24,59 +30,79 @@
       
       // get RDF data
       function fetchRdf() {
-      
+
         entity = {};
-        entity.id = $("#oclcnum").val();
-        uri = 'http://www.worldcat.org/oclc/' + entity.id;
-        requestUrl = 'http://experiment.worldcat.org/oclc/' + entity.id + '.rdf'
-        var manifestation = $rdf.sym(uri);
-        var fetch = $rdf.fetcher(kb);
-        fetch.nowOrWhenFetched(requestUrl,undefined,function(ok, body, xhr) { // @@ check ok
-          if (ok) {
-            getLdProperties(manifestation);
-          } else {
-            console.log("Fetch failed for "+requestUrl);
-          }
-        });
+        entity.id = $.trim($("#oclcnum").val());
+           
+        // test whether the requested OCLC number is an integer
+        if (Math.floor(entity.id) == entity.id && $.isNumeric(entity.id)) {
+      
+          uri = 'http://www.worldcat.org/oclc/' + entity.id;
+          requestUrl = 'http://experiment.worldcat.org/oclc/' + entity.id + '.rdf'
+          var manifestation = $rdf.sym(uri);
+          var fetch = $rdf.fetcher(kb);
+          fetch.nowOrWhenFetched(requestUrl,undefined,function(ok, body, xhr) { 
+            if (ok) {
+              getLdProperties(manifestation);
+            } else {
+              // complain and halt if the string provided for an OCLC number is not an integer
+              alert($("#oclcnum").val() + " is not recognized as an OCLC number.");
+            }
+          });
+          
+        } else {
+        
+          // complain and halt if the string provided for an OCLC number is not an integer
+          alert($("#oclcnum").val() + " is not a number.");
+          
+        }
         
       } // end function fetchRdf
       
       // get selected linked data properties
       function getLdProperties(manifestation) {
       
-        // get entity name
-        entity.name = kb.the($rdf.sym(uri), SCHEMA('name')).value;
-
-        // get creator name and uri
-        entity.creator= kb.the(kb.the(manifestation, SCHEMA('creator')), $rdf.sym('http://schema.org/name')).value;
-        entity.creatorUri = kb.the(manifestation, SCHEMA('creator')).uri;
-        
-        // handle properties that will have multiple values
-        var props = ["about","description"];
-        $.each(props, function( index, prop ) {
-          // for the schema.org about properties, add a list of their name properties to the scope
-          var propNodes = kb.each(manifestation, SCHEMA(prop));
-          var values = [];
-          for (i = 0; i < propNodes.length; i++) {
-            if (kb.the(propNodes[i], SCHEMA('name'))) {
-              values.push(kb.the(propNodes[i], SCHEMA('name')));
-            } else if (propNodes[i].value) {
-              values.push(propNodes[i].value);
+        // check to see if the graph store has a schema name propoerty
+        if (kb.the($rdf.sym(uri), SCHEMA('name'))) {
+      
+          // get entity name
+          entity.name = kb.the($rdf.sym(uri), SCHEMA('name')).value;
+          
+          // get creator name and uri
+          entity.creator= kb.the(kb.the(manifestation, SCHEMA('creator')), $rdf.sym('http://schema.org/name')).value;
+          entity.creatorUri = kb.the(manifestation, SCHEMA('creator')).uri;
+          
+          // handle properties that will have multiple values
+          var props = ["about","description"];
+          $.each(props, function( index, prop ) {
+            // for the schema.org about properties, add a list of their name properties to the scope
+            var propNodes = kb.each(manifestation, SCHEMA(prop));
+            var values = [];
+            for (i = 0; i < propNodes.length; i++) {
+              if (kb.the(propNodes[i], SCHEMA('name'))) {
+                values.push(kb.the(propNodes[i], SCHEMA('name')));
+              } else if (propNodes[i].value) {
+                values.push(propNodes[i].value);
+              }
             }
-          }
-          entity[prop] = values;
-        });
-
-        // show the entity properties in the UI
-        showLdProperties();
+            entity[prop] = values;
+          });
+          
+          // show the entity properties in the UI
+          showLdProperties();
+          
+        } else {
+        
+          // the graph doesn't include a name property, so likely did not fetch a manifestation resource
+          alert("A valid graph was not found for the OCLC number "+entity.id);
+        
+        }
         
       } // end function getLdProperties
       
       // show linked data properties
       function showLdProperties() {
-        var props = ["name","creator","about","description"];
-        $.each(props, function( pindex, prop ) {
-          $("#"+prop).html("");
+        $.each(properties, function( pindex, prop ) {
           if (entity.hasOwnProperty(prop)) {
             if (entity[prop].length > 0) {
               if ($.isArray(entity[prop])) {
